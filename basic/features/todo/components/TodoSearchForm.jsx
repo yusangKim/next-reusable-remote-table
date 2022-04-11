e6d6f93606ae4bb11435812b14fc7b3d4f8d0618
yusangKim <yoo99485@likelion.net>
2022-04-11 18:35:10 +0900

    [Update] Todo Filter add some style

--- basic/features/todo/components/TodoSearchForm.jsx
+++ basic/features/todo/components/TodoSearchForm.jsx
@@ -4,8 +4,14 @@ const TodoSearchForm = ({ searchFilter, setSearchFilter }) => {
   const [id, setId] = useState('');
 
   return (
-    <div className="border p-2">
-      <input type={'text'} value={id} onChange={(e) => setId(e.target.value)} />
+    <div className="border p-2 flex gap-5">
+      <input
+        type={'text'}
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+        placeholder="User Id"
+        className={'border-2 border-solid border-gray-300 text-l p-1'}
+      />
       <button
         onClick={(e) => {
           const filters = [];
